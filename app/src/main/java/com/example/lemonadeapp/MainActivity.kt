20579323f96f46016f84705257358548591051d2
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-09 16:38:25 -0700

    added the logic for the when conditional

--- app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonadeapp.ui.theme.LemonadeAppTheme
 
@@ -23,16 +33,96 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
-    @Composable
-    fun lemonadeApp() {
-    }
+@Composable
+fun LemonadeApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun LemonadeAppPreview() {
-        LemonadeAppTheme {
-            LemonadeApp()
+    when (currentStep) {
+        1 -> {
+            LemonadeWithButtonAndImage(
+                textLabelResourceId = R.string.lemonade_select,
+                drawableResourceId = R.drawable.lemon_tree,
+                contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                OnImageClick = {
+                    currentStep = 2
+                    squeezeCount = (2..4).random()
+                }
+            )
         }
+
+        2 -> {
+            LemonadeWithButtonAndImage(
+                textLabelResourceId = R.string.lemonade_squeeze,
+                drawableResourceId = R.drawable.lemon_squeeze,
+                contentDescriptionResourceId = R.string.lemon_content_description,
+                OnImageClick = {
+                    squeezeCount--
+                    if (squeezeCount == 0) {
+                        currentStep = 3
+                    }
+                }
+            )
+        }
+
+        3 -> {
+            LemonadeWithButtonAndImage(
+                textLabelResourceId = R.string.lemonade_drink,
+                drawableResourceId = R.drawable.lemon_drink,
+                contentDescriptionResourceId = R.string.lemonade_content_description,
+                OnImageClick = {
+                    currentStep = 4
+                }
+            )
+        }
+
+        4 -> {
+            LemonadeWithButtonAndImage(
+                textLabelResourceId = R.string.lemonade_empty,
+                drawableResourceId = R.drawable.lemon_restart,
+                contentDescriptionResourceId = R.string.empty_glass_content_description,
+                OnImageClick = {
+                    currentStep = 1
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun LemonadeWithButtonAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    OnImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(modifier = modifier) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Button(
+                onClick = OnImageClick,
+                Modifier.wrapContentSize()
+            ) {
+                Image(
+                    painter = painterResource(drawableResourceId),
+                    contentDescription = stringResource(contentDescriptionResourceId),
+                    modifier = Modifier
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LemonadeAppPreview() {
+    LemonadeAppTheme {
+        LemonadeApp()
     }
 }
